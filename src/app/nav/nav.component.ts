19ff7380141e5ad1c8ffe8b7ec0f28f869c50dfa
arbiva <arbiva@post.bgu.ac.il>
2023-01-16 11:39:24 +0200

    auth

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
-  constructor(public modal: ModalService) { }
-
-  ngOnInit(): void {
+  isAuthenticated = false;
+  constructor(public modal: ModalService, public auth: AuthService) {
+    this.auth.isAuthenticated$.subscribe(status => {
+      this.isAuthenticated = status;
+    });
   }
+  ngOnInit(): void {}
 
-  openModal($event: Event){
-    $event.preventDefault()
-    
-    this.modal.toggleModal('auth')
+  openModal($event: Event) {
+    $event.preventDefault();
+
+    this.modal.toggleModal('auth');
   }
-
 }
